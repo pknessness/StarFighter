8533216ca1f1647972db5c1e0265fd8e73d75ef5
Anshal Jain <pk.ness.ness@gmail.com>
2024-09-06 05:21:06 -0700

    rando colors

--- ray_main.cpp
+++ ray_main.cpp
@@ -2,6 +2,9 @@
 extern "C" {
     #include "raylib5\include\raylib.h"
 }
+#include <cmath>
+#include <stdint.h>
+#include <stdio.h>
 // #include "raylib5\include\raylib.h"
 
 // int main() {
@@ -33,23 +36,100 @@ extern "C" {
 
 //     return 0;
 // }
-const int pixelScale = 20;
+int pixelScale = 20;
+int screenWidth = 800;
+int screenHeight = 600;
+
+int randSeed = 0;
+
+float randomValue(int in){
+    long val = (in*(in*2140 + 21390) * (in * 2139021 + 213908)) & 0xFFFFFF;
+    return val/0xFFFFFF;
+}
+
+uint8_t randomColor(int x, int y, int a){
+    int in = (x * screenWidth + y) * (a+434);
+    long val = (in*(in*2140 + 21390) * (in * 2139021 + 213908)) & 0xFFFFFF;
+    printf("in:%d val:%d\n", in, val);
+    return val >> 16;
+}
+
+struct vector3 {
+    float x, y, z;
+
+    vector3(float x, float y, float z) 
+        : x(x), y(y), z(z)
+    {
+    }
+    
+    // addop. doesn't modify object. therefore const.
+    vector3 operator+(const vector3& a)
+    {
+        return vector3(a.x+x, a.y+y, a.z+z);
+    } 
+
+    vector3 operator-(const vector3& a)
+    {
+        return vector3(a.x+x, a.y+y, a.z+z);
+    } 
+
+    float dot(const vector3& a)
+    {
+        return x*a.x + y*a.y, z*a.z;
+    }
+
+    vector3 cross(const vector3& a)
+    {
+        return vector3(y*a.z - z*a.y,z*a.x - x*a.z,x*a.y - y*a.x);
+    }
+
+    vector3 norm()
+    {
+        float mag = sqrt(x*x + y*y + z*z);
+        return vector3(x/mag,y/mag,z/mag);
+    }
+
+    float mag()
+    {
+        return sqrt(x*x + y*y + z*z);
+    }
+};
+
+struct location {
+    vector3 pos;
+    vector3 fwdVector;
+    vector3 rightVector;
+};
+
+struct Cam{
+    location loc; //back core of the camera
+    float unitsPerPixelVertical;
+    float pixelWidth;
+    float pixelHeight;
+    float focalLength;
+};
 
 void drawScaledPixel(int posX, int posY, Color color){
     // DrawPixel(0*posX)
-    DrawRectangle(0 * posX, 0 * posY, pixelScale - 1, pixelScale - 1, color);
+    DrawRectangle(pixelScale * posX, pixelScale * posY, pixelScale, pixelScale, color);
 }
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Raylib basic window");
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         // DrawText("It works!", 20, 20, 20, BLACK);
-        drawScaledPixel(0,0,(Color){0x76,0x26,0x38,0x77});
+        for(int i = 0; i < screenWidth/pixelScale; i ++){
+            for(int j = 0; j < screenHeight/pixelScale; j ++){
+                uint8_t r = randomColor(i, j, 0);
+                uint8_t g = randomColor(i, j, 1);
+                uint8_t b = randomColor(i, j, 2);
+                // printf("%d %d %d", r, g, b);
+                drawScaledPixel(i,j,(Color){r,g,b,255});
+            }
+        }
         EndDrawing();
     }
     CloseWindow();
